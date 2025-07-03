5b19e3da0687e455e27cbea4b14db98784561ede
AngelDown12 <angeldown12xz@gmail.com>
2025-07-02 21:25:01 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,1 +1,74 @@
-//xd
+// Código creado por Deylin
+// https://github.com/Deylin-eliac 
+// codigo creado para https://github.com/Deylin-eliac/Pikachu-bot 
+// No quites créditos
+
+import PhoneNumber from 'awesome-phonenumber';
+
+let handler = async (m, { conn }) => {
+  m.react('🎭');
+
+const imageUrl = ''
+  const numCreador = '5218991555766';
+  const ownerJid = numCreador + '@s.whatsapp.net';
+  const name = await conn.getName(ownerJid) || 'Alee';
+  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || `𝐒𝐨𝐲 𝐑𝐞𝐲𝐞𝐬 𝐭𝐥𝐞 𝐭𝐥𝐞 𝐭𝐥𝐞.`;
+  const empresa = '𝐑𝐞𝐲𝐞𝐬 𝐭𝐥𝐞 𝐭𝐥𝐞 𝐭𝐥𝐞 - 𝐒𝐞𝐫𝐯𝐢𝐜𝐢𝐨𝐬 𝐭𝐞𝐜𝐧𝐨𝐥𝐨𝐠𝐢𝐜𝐨𝐬';
+
+  const vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:https://www.tuempresa.com
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD`.trim();
+
+
+  await conn.sendMessage(
+    m.chat,
+    {
+      contacts: {
+        displayName: name,
+        contacts: [{ vcard }]
+      },
+      contextInfo: {
+        mentionedJid: [m.sender],
+        isForwarded: true,
+        forwardingScore: 999,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: 'Alee'.id,
+          newsletterName: 'Alee'.name,
+          serverMessageId: -1,
+        },
+        externalAdReply: {
+          title: textbot,
+          body: dev,
+          thumbnailUrl: 'https://qu.ax/GbxoW.jpg',
+          sourceUrl: 'oo',
+          mediaType: 1,
+          showAdAttribution: true,
+          renderLargerThumbnail: true,
+        },
+      }
+    },
+    { quoted: m }
+  );
+}
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
