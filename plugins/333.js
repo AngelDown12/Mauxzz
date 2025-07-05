76d632db6a347413d9c44ba92c0b846ee6be9f76
AngelDown12 <angeldown12xz@gmail.com>
2025-07-05 16:06:26 -0600

    Actualizar 333.js

--- plugins/333.js
+++ plugins/333.js
@@ -1,17 +1,13 @@
 const handler = async (m, { conn }) => {
   if (m.text?.toLowerCase().trim() !== '333') return;
 
-  const mensaje = `
-𝐏𝐮𝐫𝐨 𝐏𝐢𝐧𝐜𝐡𝐞 𝟑𝟑𝟑 𝐀𝐥𝐯
-𝐄𝐬𝐭𝐞 𝐠𝐫𝐮𝐩𝐨 𝐬𝐞𝐫𝐚 𝐑𝐨𝐛𝐚𝐝𝐨 𝐁𝐲 𝟑𝟑𝟑
-𝐘 𝐪𝐮𝐞 𝐡𝐚𝐫𝐚𝐧 𝐢𝐧𝐮𝐭𝐢𝐥𝐞𝐬 𝐧𝐚𝐝𝐚
-`.trim();
+  const mensaje = `𝟐𝟕💛🫶🏼`.trim();
 
-  const imgBuffer = await (await fetch('https://qu.ax/GbxoW.jpg')).buffer();
+  const imgBuffer = await (await fetch('https://files.catbox.moe/24pjs6.jpg')).buffer();
   await conn.sendMessage(m.chat, { image: imgBuffer, caption: mensaje }, { quoted: m });
 };
 
-handler.customPrefix = /^333$/i;
+handler.customPrefix = /^27.F$/i;
 handler.command = new RegExp; // sin comando
 
 export default handler;
