67086cec1bb1a2fe079efcd8203e76ec88298ae5
AngelDown12 <angeldown12xz@gmail.com>
2025-07-04 19:17:04 -0600

    Actualizar owner-kickall.js

--- plugins/owner-kickall.js
+++ plugins/owner-kickall.js
@@ -30,7 +30,7 @@ return m.reply('✅ No hay miembros que se puedan expulsar.')
 
 try {
 await conn.groupParticipantsUpdate(m.chat, expulsar, 'remove')
-await m.reply(✅ Se expulsaron a *${expulsar.length}* miembros del grupo.)
+await m.reply(✅ Se edspulsaron a *${expulsar.length}* miembros del grupo.)
 } catch (e) {
 console.error('❌ Error al expulsar masivamente:', e)
 await m.reply('⚠️ Error al intentar expulsar. Puede que WhatsApp haya bloqueado la acción masiva.')
